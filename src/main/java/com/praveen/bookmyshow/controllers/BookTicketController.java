abb51c373dcf3ea7fcb22dbb5b96e2016fc8a07d
Praveen <praveengupta86012@gmail.com>
2023-12-14 01:27:53 +0530

    done with book ticket

--- src/main/java/com/praveen/bookmyshow/controllers/BookTicketController.java
+++ src/main/java/com/praveen/bookmyshow/controllers/BookTicketController.java
@@ -4,6 +4,7 @@ import com.praveen.bookmyshow.dtos.BookTicketRequestDto;
 import com.praveen.bookmyshow.dtos.BookTicketResponseDto;
 import com.praveen.bookmyshow.exceptions.InvalidArgumentsException;
 import com.praveen.bookmyshow.exceptions.SeatNotAvailableException;
+import com.praveen.bookmyshow.models.Ticket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.praveen.bookmyshow.services.TicketService;
@@ -26,13 +27,15 @@ public class BookTicketController {
     public BookTicketResponseDto bookTicket(BookTicketRequestDto bookTicketRequestDto) {
         BookTicketResponseDto bookTicketResponseDto = new BookTicketResponseDto();
         try{
-            ticketService.bookTicket(
+            Ticket ticket = ticketService.bookTicket(
                     bookTicketRequestDto.getUserId(),
                     bookTicketRequestDto.getShowId(),
                     bookTicketRequestDto.getSeatIds()
             );
             bookTicketResponseDto.setStatus("SUCCESS");
             bookTicketResponseDto.setMessage("Ticket booked successfully");
+            bookTicketResponseDto.setTicketId(ticket.getId());
+            bookTicketResponseDto.setAmount(ticket.getAmount());
         }
         catch (InvalidArgumentsException | SeatNotAvailableException e) {
             throw new RuntimeException(e);
--- src/main/java/com/praveen/bookmyshow/dtos/BookTicketResponseDto.java
+++ src/main/java/com/praveen/bookmyshow/dtos/BookTicketResponseDto.java
@@ -1,6 +1,5 @@
 package com.praveen.bookmyshow.dtos;
 
-import com.praveen.bookmyshow.models.Ticket;
 import lombok.Getter;
 import lombok.Setter;
 
